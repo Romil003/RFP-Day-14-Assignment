3e44f15c677cda5f18b782f90b20485e0676539b
Romil003 <romilghadge33@gmail.com>
2022-10-29 14:05:37 +0530

    Added Linked List UC2

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,17 +1,16 @@
 package com.bridgelabz;
 
-import java.util.LinkedList;
-
 public class LinkedListMain {
 
     public static void main(String[] args) {
 
-        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        com.bridgelabz.LinkedList<Integer> linkedList1 = new LinkedList<>();
 
         linkedList1.add(56);
         linkedList1.add(30);
         linkedList1.add(70);
 
-        System.out.println("Linked List => " + linkedList1);
+        linkedList1.display();
+
     }
 }
